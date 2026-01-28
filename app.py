10f63595521a6cd4b32fae059547e6a6c335c30f
KakeguriMashoBayou <boyul@nycstudents.net>
2026-01-28 09:22:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+def  rectangle(x):
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+rectangle(400)
